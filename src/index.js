22603ae51278d3a737263e7c47d49d2aea1be64f
unknown <aelrhman75@yahoo.com>
2021-01-25 17:34:54 +0200

    j

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter} from "react-router-dom";
+import { HashLink } from 'react-router-hash-link';
+
 
 
 ReactDOM.render(
   <HashRouter >
+    <HashLink>
     <App />
+    </HashLink>
   </HashRouter>,
   document.getElementById('root')
 );
